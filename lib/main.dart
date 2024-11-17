9e645ede15267e197312ecf9866bfd93c5e6f43a
Mubashshir Ali <jssmithsa6516@gmail.com>
2024-11-17 17:31:16 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() = >runApp(new myApp());
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
